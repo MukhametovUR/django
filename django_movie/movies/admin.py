686d0a845803ec3e4c56fd7d8a5e24548dd8b8f8
MukhametovUR <ural-lan4x4@yandex.ru>
2022-07-08 17:49:08 +0500

    create admin panel with supertuser

--- django_movie/movies/admin.py
+++ django_movie/movies/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Category, Genre, Movie, MovieShots, Actor, Rating, RatingStar, Reviews
+
+#Региструруем мадели в админ.панели Django
+admin.site.register(Category)
+admin.site.register(Genre)
+admin.site.register(Movie)
+admin.site.register(MovieShots)
+admin.site.register(Actor)
+admin.site.register(Rating)
+admin.site.register(RatingStar)
+admin.site.register(Reviews)
