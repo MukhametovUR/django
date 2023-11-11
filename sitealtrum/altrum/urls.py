54170a14a338a7c46a76524840b5c4ffbbca0c41
Ural <amgid7@mail.ru>
2023-11-11 22:26:40 +0500

    added redirect

--- sitealtrum/altrum/urls.py
+++ sitealtrum/altrum/urls.py
@@ -4,9 +4,9 @@ from . import converters
 register_converter(converters.FourDigitYearConverter, "year4")
 
 urlpatterns = [
-    path('', views.index), #http://127.0.0.1:8000/
-    path('cats/<int:cat_id>/', views.categories), #http://127.0.0.1:8000/categories/
-    path('cats/<slug:cat_slug>/', views.categories_by_slug),  # http://127.0.0.1:8000/categories/
+    path('', views.index, name='home'), #http://127.0.0.1:8000/
+    path('cats/<int:cat_id>/', views.categories, name='cats_id'), #http://127.0.0.1:8000/categories/
+    path('cats/<slug:cat_slug>/', views.categories_by_slug, name='cats'),  # http://127.0.0.1:8000/categories/
     # re_path(r"^archive/(?P<year>[0-9]{4})/", views.archive)
-    path("archive/<year4:year>/", views.archive)
+    path("archive/<year4:year>/", views.archive, name='archive')
 ]
--- sitealtrum/altrum/views.py
+++ sitealtrum/altrum/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 
 
 def index(request):
@@ -17,8 +18,10 @@ def categories_by_slug(request, cat_slug):
 
 def archive(request, year):
     if year > 2023:
-        raise Http404()
+        uri = reverse('home')
+        return redirect(uri)
     return HttpResponse(f"<h1>Архив по годам</h1><p>{year}</p>")
+
 
 def page_not_found(request, exception):
     return HttpResponseNotFound("<h1>Страница не найдена</h1>")
