fabbbd5b8d8763926e20d17b4fcc84fb9fe4f5ee
Ural <amgid7@mail.ru>
2023-12-10 00:23:28 +0500

    new class category

--- sitealtrum/altrum/models.py
+++ sitealtrum/altrum/models.py
@@ -19,6 +19,7 @@ class Altrum(models.Model):
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(choices=Status.choices, default=Status.DRAFT)
+    cat = models.ForeignKey('Category', on_delete=models.CASCADE)
 
     objects = models.Manager()
     published = PublishedManager()
@@ -34,3 +35,11 @@ class Altrum(models.Model):
 
     def get_absolute_url(self):
         return reverse('post', kwargs={'post_slug': self.slug})
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, db_index=True)
+    slug = models.SlugField(max_length=255, unique=True, db_index=True)
+
+    def __str__(self):
+        return self.name
